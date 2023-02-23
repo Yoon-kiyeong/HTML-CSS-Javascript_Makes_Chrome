c88dc786efa56b7f36ac5dd816676c839a60f05b
Yoon-kiyeong <kyyoon2105@naver.com>
2023-02-23 18:05:40 +0900

    4-0 Input Values
    2023-02-23

---
+++ Webcontent/4-0/app.js
@@ -0,0 +1,14 @@
+// const loginForm = document.getElementById("#login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+//document 또는 하나의 element를 통해서 검색이 가능하다. 오직 한 element에서 찾을 수 있다.
+
+function handleLoginBtnClick() {
+    console.log(loginInput.value);    
+}
+
+loginButton.addEventListener("click",handleLoginBtnClick)
+
