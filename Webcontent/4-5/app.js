f46d86042aaf13ff7e47587fd3e79e6581453ef3
Yoon-kiyeong <kyyoon2105@naver.com>
2023-04-19 17:02:41 +0900

    4-5 Saving Username
    2023-04-19

---
+++ Webcontent/4-5/app.js
@@ -0,0 +1,18 @@
+const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    console.log(username);
+    localStorage.setItem("username", username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);
