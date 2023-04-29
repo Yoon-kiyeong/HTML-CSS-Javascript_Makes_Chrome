a587e384e9fec7d6399ef69688465a69c3e42812
Yoon-kiyeong <kyyoon2105@naver.com>
2023-04-29 17:37:49 +0900

    7-7 Deleting To Dos Part One
    2023-04-29

---
+++ Webcontent/7-7/js/todo.js
@@ -0,0 +1,53 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+let toDos = [];
+
+const TODOS_KEY = "todos";
+
+function saveToDos() {
+    localStorage.setItem("todos",JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    console.log(li.id);
+    li.remove();
+}
+
+function paintToDo(newTodo) {
+    const list = document.createElement("li");
+    list.id = newTodo.id;
+    const list_span = document.createElement("span");
+    list_span.innerText = newTodo.text;
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+    list.appendChild(list_span);
+    list.appendChild(button);
+    toDoList.appendChild(list);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    const newTodoObj = {
+        text:newTodo,
+        id: Date.now(),
+    }
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
+    saveToDos();
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
