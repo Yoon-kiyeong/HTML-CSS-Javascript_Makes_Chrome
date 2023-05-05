2cfd8a5b3a1cbce40e0ebf2e72b10d0cb8ae84b4
Yoon-kiyeong <kyyoon2105@naver.com>
2023-05-05 18:40:17 +0900

    2-3 Variables
    2023-05-05
    repository 정리용

---
+++ Webcontent/2-3/app.js
@@ -0,0 +1,16 @@
+console.log(545454);
+console.log("lalala");
+console.log('lalala');
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+
+const a = 5;
+const b = 2;
+const myName = "Yoon";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
