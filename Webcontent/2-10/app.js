6c2f17aec002ee7430c4a0a96aa719f1e6b8c1fe
Yoon-kiyeong <kyyoon2105@naver.com>
2023-05-05 19:10:14 +0900

    2-10 Recap
    2023-05-05
    repository 정리용

---
+++ Webcontent/2-10/app.js
@@ -0,0 +1,23 @@
+const calculator = {
+    add: function(a,b){
+    console.log(a+b);
+    },
+    min: function(a,b){
+    console.log(a-b);
+    },
+    mul: function(a,b){
+    console.log(a*b);
+    },
+    div: function(a,b){
+    console.log(a/b);
+    },
+    power: function(a,b){
+    console.log(a**b)
+    },
+    };
+
+    calculator.add(5,5);
+    calculator.min(10,5);
+    calculator.mul(5,5);
+    calculator.div(20,4);
+    calculator.power(2,2);
