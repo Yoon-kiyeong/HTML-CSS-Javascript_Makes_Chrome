ad5e996b5bddbd5ae36eafc34c9e93d3386b817c
Yoon-kiyeong <kyyoon2105@naver.com>
2023-05-05 18:43:58 +0900

    2-4 const and let
    2023-05-05
    repository 정리용

---
+++ Webcontent/2-4/app.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 2;
+let myName = "Yoon";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log("hello " + myName);
+
+myName = "YoonKiYeong";
+
+console.log("your fullname is " + myName);
