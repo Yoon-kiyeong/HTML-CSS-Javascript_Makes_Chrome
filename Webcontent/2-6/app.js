052f2df27179868dfe288b46f1d67652be9013e3
Yoon-kiyeong <kyyoon2105@naver.com>
2023-05-05 18:54:54 +0900

    2-6 Arrays
    2023-05-05
    repository 정리용

---
+++ Webcontent/2-6/app.js
@@ -0,0 +1,12 @@
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+const nonsense = [1,2,"hello",false,null,true,undefined,"Yoon"];
+
+//Get Item from Array
+console.log(daysOfWeek);
+console.log(nonsense);
+console.log(daysOfWeek[5]);
+
+//Add one more day to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
